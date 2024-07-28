f67b919d75e761001b8dfec3559fd17980f0f04c
art <artem@reply.io>
2024-07-28 16:50:11 +0300

    fix post request

--- server/index.ts
+++ server/index.ts
@@ -13,7 +13,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(cors())
 app.use('/api', api)
